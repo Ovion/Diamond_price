624d864a2f78ac451f0c9e3342332232324af667
Ovion <ovi2825@gmail.com>
2019-12-07 21:41:30 +0100

    primeras pruebas

---
+++ src/constants/constants.py
@@ -0,0 +1,11 @@
+
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+PATH_DATA = "../../input/data.csv"
+PATH_TRAIN = "../../input/test.csv"
+
+PATH_DATA_CLEAN = "../../input/clean_data.csv"
+PATH_TRAIN_CLEAN = "../../input/clean_test.csv"
