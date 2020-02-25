20d435dfd188aef87fdbf3f2c86f56092030b6e5
Ovion <ovi2825@gmail.com>
2020-02-25 13:20:04 +0100

    Saludos alumn@ de datamad 0120, intentando ganar la competición de Kaggle ehhh

--- src/approches/first_look.py
+++ src/approches/first_look.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
@@ -27,20 +28,34 @@ from sklearn.svm import SVR
 
 from sklearn.multioutput import MultiOutputRegressor
 
+'''
+Hola... Qué mirando a ver cómo ganas la competición de Kaggle??? Solo decir que yo no la gané
+Pero en archivos ocultos estoy intentando ganaros a vosotros
+Muwahahahahahah
+
+No obstante he retocado este archivo para que tengáis algo muy útil de por donde tirar
+'''
+
 
 data = pd.read_csv('input/clean_data_num.csv')
 
 X = data.drop(['price'], axis=1)
 y = data.price
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 models = {
-    # 'LR': LinearRegression(),
-    'RFR': RandomForestRegressor(n_estimators=100),
-    'MOR': MultiOutputRegressor(RandomForestRegressor(n_estimators=100)),
+    # 'LR': LinearRegression(n_jobs=-1),
+    # 'RFR0': RandomForestRegressor(n_estimators=100, n_jobs=-1, criterion='mae'),
+    'RFR1': RandomForestRegressor(n_estimators=100, n_jobs=-1),
+    # 'KNR0': KNeighborsRegressor(n_jobs=-1, weights='distance'),
+    # 'KNR1': KNeighborsRegressor(n_jobs=-1, weights='uniform'),
+    # 'KNR2': KNeighborsRegressor(n_jobs=-1, weights='uniform', algorithm='ball_tree'),
+    # 'KNR3': KNeighborsRegressor(n_jobs=-1, weights='uniform', algorithm='kd_tree'),
+    # 'KNR4': KNeighborsRegressor(n_jobs=-1, weights='uniform', algorithm='brute'),
+    # 'MOR': MultiOutputRegressor(RandomForestRegressor(n_estimators=100)),
     # 'DTR': DecisionTreeRegressor(),
-    # 'KNR': KNeighborsRegressor(),
-    # 'GBR': GradientBoostingRegressor(),
+    'GBR': GradientBoostingRegressor(validation_fraction=0.2),
     # 'ABR': AdaBoostRegressor(),
     # 'GPR': GaussianProcessRegressor(),
     # 'MNB': MultinomialNB(),
@@ -52,13 +67,36 @@ models = {
 
 for model_name, model in models.items():
     print(f'Training model: {model_name}')
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-    rmse = mean_squared_error(y_test, y_pred)
-    params = model.get_params
+    # Entrenando para cada modelo del diccionario
+    lst_success = []
+    lst_rmse = []
+    for i in range(6):
+        # Este es mi propio Cross Validation
+        print(f'Iteration number {i}')
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2)
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
 
-    print('Saving params in records.txt ...')
+        dpre = pd.DataFrame({
+            'pred': y_pred,
+            'GT': y_test})
+
+        success = round(
+            (dpre[dpre.pred == dpre.GT].shape[0]/len(dpre.pred))*100, 2)
+        lst_success.append(success)
+        # Esta es una lista donde voy guardando lo que he acertado (Acertar el precio exacto es prácticamente imposible)
+        lst_rmse.append(mean_squared_error(y_test, y_pred)**0.5)
+        # Esta es una lista donde voy almacenando el error cuadrático medio
+
+    print("Saving data at 'output/records.txt'...")
+    # Tengo un log de todo lo que voy haciendo
+    success_mean = round(sum(lst_success)/len(lst_success), 2)
+    rmse_mean = round(sum(lst_rmse)/len(lst_rmse), 2)
+    params = model.get_params
     with open('output/records.txt', "a+") as file:
         file.write(
-            f'''Model: {model_name}\t RMSE: {rmse}\t Params: {params} \n\n'''
+            f'''Model: {model_name}\t Success: {success_mean}%\t RMSE: {rmse_mean}%
+            \n\tParams: {params} \n\n'''
         )
+    print(f'Model {model_name} analyzed')
--- src/cleaner_trans/cleaning.py
+++ src/cleaner_trans/cleaning.py
@@ -53,6 +53,7 @@ def magic_size(value):
 def apply_magic_clean(df):
     df.drop(['x', 'y', 'z'], axis=1, inplace=True)
     # df['magic'] = df.carat.apply(lambda x: magic_size(x))
+    df.carat = df.carat.apply(lambda x: x**3)
     df.clarity = df.clarity.apply(lambda x: num_clarity(x))
     df.color = df.color.apply(lambda x: num_color(x))
     df.cut = df.cut.apply(lambda x: num_cut(x))
